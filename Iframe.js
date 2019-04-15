799b70bc5eb1c1ca31909924b1a5d53864414e02
Felipe Maciel <felipemrvieira@gmail.com>
2019-04-15 14:12:22 -0300

    Cria componente iframe

--- Iframe.js
+++ Iframe.js
@@ -1,1 +1,23 @@
-sd
+import React, { Component } from 'react';
+import './Iframe.scss';
+
+
+class Iframe extends Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return (
+            <iframe 
+                className="component-iframe" 
+                width={this.props.width} 
+                height={this.props.height}
+                src={this.props.src} 
+            />
+        );
+    }
+}
+
+export default Iframe;
+
+
